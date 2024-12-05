54eed71f32b1ed0494ff6633681b3f5eacbfa52e
Simon Demaziere <simon.demaziere@insee.fr>
2024-12-05 22:17:05 +0100

    fix: add log for debug

--- src/main/java/fr/insee/sabianedata/ws/service/xsl/XslTransformation.java
+++ src/main/java/fr/insee/sabianedata/ws/service/xsl/XslTransformation.java
@@ -1,5 +1,8 @@
 package fr.insee.sabianedata.ws.service.xsl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -14,16 +17,25 @@ import javax.xml.transform.stream.StreamSource;
  *
  * @author gerose
  */
+@Service
+@Slf4j
 public class XslTransformation {
 
+    private final TransformerFactory tFactory = TransformerFactory.newInstance();
 
-    public void xslTransform(Transformer transformer, InputStream xmlInput, OutputStream xmlOutput) throws Exception {
+    private void xslTransform(Transformer transformer, InputStream xmlInput, OutputStream xmlOutput) throws Exception {
+        try {
+
         transformer.transform(new StreamSource(xmlInput), new StreamResult(xmlOutput));
+        }catch (Exception e){
+            log.warn("Error when transforming",e);
+            throw new Exception("Transformation failed");
+        }
     }
 
     public void transformFods2XML(InputStream inputFile, OutputStream outputFile, InputStream xslSheet) throws Exception {
-        TransformerFactory tFactory = TransformerFactory.newInstance();
-        System.out.println("TransformerFactory: " + tFactory.getClass().getName());
+
+        log.info("TransformerFactory: {} " , tFactory.getClass().getName());
         Transformer transformer = tFactory.newTransformer(new StreamSource(xslSheet));
         xslTransform(transformer, inputFile, outputFile);
     }
